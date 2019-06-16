2b525842d39d130b7e36d43d9273c10f39ceee7d
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2019-06-16 12:32:50 +0200

    start implementing autocomplete

--- ASCIImage/ASCIImage/Program.cs
+++ ASCIImage/ASCIImage/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ASCIImage
@@ -9,22 +12,97 @@ namespace ASCIImage
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0) return;
+            getpath();
+            string file = "";
             try
             {
-                using (StreamWriter s = new StreamWriter(args[0] + ".txt", false))
+                file = args[0];
+            }
+            catch
+            {
+                Console.Write($"Insert a file path: ");
+                file = Console.ReadLine();
+            }
+                
+            while (!File.Exists(file))
+            {
+                Console.Write($"'{file}' doesn't exists. Insert a file path: ");
+                file = Console.ReadLine();
+            }
+            try
+            {
+                using (StreamWriter s = new StreamWriter(file + ".txt", false))
                 {
-                    s.Write(ImageConverter.GetImage(args[0]));
+                    s.Write(ImageConverter.GetImage(file, 1));
                     s.Close();
                 }
             }
             catch
             {
-                try { File.Delete(args[0] + ".txt"); } catch { }
+                try { File.Delete(file + ".txt"); } catch { }
                 Console.WriteLine("something happend!");
             }
             Console.WriteLine("press any key to exit");
             Console.ReadKey();
+        }
+
+        static void getpath()
+        {
+            void ClearCurrentLine()
+            {
+                var currentLine = Console.CursorTop;
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currentLine);
+            }
+
+            var builder = new StringBuilder();
+            var input = Console.ReadKey(intercept:true);
+
+            while (input.Key != ConsoleKey.Enter)
+            {
+                var currentInput = builder.ToString();
+                if (input.Key == ConsoleKey.Tab)
+                {
+                    List<string> data = new List<string>();
+                    string raw = builder.ToString();
+                    string direc = string.Join("\\", raw.Split('\\').Reverse().Skip(1).Reverse()) + "\\";
+                    data = Directory.GetFiles(direc).ToList();
+                    data.AddRange(Directory.GetDirectories(direc));
+                    var match = data.FirstOrDefault(item => item != currentInput && item.StartsWith(currentInput, true, CultureInfo.InvariantCulture));
+                    if (string.IsNullOrEmpty(match))
+                    {
+                        input = Console.ReadKey(intercept: true);
+                        continue;
+                    }
+
+                    ClearCurrentLine();
+                    builder.Clear();
+
+                    Console.Write(match);
+                    builder.Append(match);
+                }
+                else
+                {
+                    if (input.Key == ConsoleKey.Backspace && currentInput.Length > 0)
+                    {
+                        builder.Remove(builder.Length - 1, 1);
+                        ClearCurrentLine();
+
+                        currentInput = currentInput.Remove(currentInput.Length - 1);
+                        Console.Write(currentInput);
+                    }
+                    else
+                    {
+                        var key = input.KeyChar;
+                        builder.Append(key);
+                        Console.Write(key);
+                    }
+                }
+
+                input = Console.ReadKey(intercept:true);
+            }
+            Console.Write(input.KeyChar);
         }
     }
 
@@ -32,11 +110,10 @@ namespace ASCIImage
     {
         private const int char_x = 1;
         private const int char_y = 2;
-        private const int parts = 5;
         private const string brightness = "@##$||===;;::''''...  ";
 
         private ImageConverter() { }
-        public static string GetImage(string file)
+        public static string GetImage(string file, int parts = 5)
         {
             Bitmap image = new Bitmap(Image.FromFile(file));
             decimal[,] values = new decimal[(int)Math.Ceiling((float)image.Width / char_x / parts), (int)Math.Ceiling((float)image.Height / char_y / parts)];
