04df799a975cffcf9004171ea598afdadf08b11c
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2019-06-16 11:28:15 +0200

    using Stringbuilder

--- ASCIImage/ASCIImage/Program.cs
+++ ASCIImage/ASCIImage/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.IO;
+using System.Text;
 
 namespace ASCIImage
 {
@@ -15,7 +14,7 @@ namespace ASCIImage
             {
                 using (StreamWriter s = new StreamWriter(args[0] + ".txt", false))
                 {
-                    s.Write(new ImageConverter(args[0]).GetImage());
+                    s.Write(ImageConverter.GetImage(args[0]));
                     s.Close();
                 }
             }
@@ -34,35 +33,34 @@ namespace ASCIImage
         private const int char_x = 1;
         private const int char_y = 2;
         private const int parts = 5;
-        private const string bright = "@##$===;:::''''... ";
-        private Bitmap i { get; set; }
-        private decimal[,] val { get; set; }
+        private const string brightness = "@##$||===;;::''''...  ";
 
-        public ImageConverter(string args) => i = new Bitmap(Image.FromFile(args));
-        public string GetImage()
+        private ImageConverter() { }
+        public static string GetImage(string file)
         {
-            val = new decimal[(int)Math.Ceiling((float)i.Width / char_x / parts), (int)Math.Ceiling((float)i.Height / char_y / parts)];
-            for (int x = 0; x < i.Width; x++)
+            Bitmap image = new Bitmap(Image.FromFile(file));
+            decimal[,] values = new decimal[(int)Math.Ceiling((float)image.Width / char_x / parts), (int)Math.Ceiling((float)image.Height / char_y / parts)];
+            for (int x = 0; x < image.Width; x++)
             {
-                for (int y = 0; y < i.Height; y++)
+                for (int y = 0; y < image.Height; y++)
                 {
-                    val[(int)Math.Floor((float)x / char_x / parts), (int)Math.Floor((float)y / char_y / parts)] += (decimal)i.GetPixel(x, y).GetBrightness() / parts / parts / char_x / char_y;
+                    values[(int)Math.Floor((float)x / char_x / parts), (int)Math.Floor((float)y / char_y / parts)] += (decimal)image.GetPixel(x, y).GetBrightness() / parts / parts / char_x / char_y;
                 }
-                Console.Write($"\rprogress: {Math.Ceiling(100f * x / i.Width)}%");
+                Console.Write($"\rprogress: {Math.Ceiling(100f * x / image.Width)}%");
             }
             Console.WriteLine();
-            string output = string.Empty;
-            for (int y = 0; y < (int)Math.Ceiling((float)i.Height / char_y / parts); y++)
+            StringBuilder sb = new StringBuilder();
+            for (int y = 0; y < (int)Math.Ceiling((float)image.Height / char_y / parts); y++)
             {
-                for (int x = 0; x < (int)Math.Ceiling((float)i.Width / char_x / parts); x++)
+                for (int x = 0; x < (int)Math.Ceiling((float)image.Width / char_x / parts); x++)
                 {
-                    output += bright[(int)((float)val[x, y] * bright.Length)];
+                    sb.Append(brightness[(int)((float)values[x, y] * brightness.Length)]);
                 }
-                output += "\n";
-                Console.Write($"\rprogress: {Math.Ceiling(100f * y / (int)Math.Ceiling((float)i.Height / char_y / parts))}%");
+                sb.Append(Environment.NewLine);
+                Console.Write($"\rprogress: {Math.Ceiling(100f * y / (int)Math.Ceiling((float)image.Height / char_y / parts))}%");
             }
             Console.WriteLine("\nDONE!");
-            return output;
+            return sb.ToString();
         }
     }
 }
