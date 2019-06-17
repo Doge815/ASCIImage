5e16d6ad3b4441777dc470299f8a9ad7edfb6785
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2019-06-17 12:13:46 +0200

    improve auto complete

--- ASCIImage/ASCIImage/Program.cs
+++ ASCIImage/ASCIImage/Program.cs
@@ -12,7 +12,7 @@ namespace ASCIImage
     {
         static void Main(string[] args)
         {
-            getpath();
+            AutoComplete.GetPath();
             string file = "";
             try
             {
@@ -40,104 +40,12 @@ namespace ASCIImage
             catch
             {
                 try { File.Delete(file + ".txt"); } catch { }
-                Console.WriteLine("something happend!");
+                Console.WriteLine("something happened!");
             }
             Console.WriteLine("press any key to exit");
             Console.ReadKey();
         }
 
-        static void getpath()
-        {
-            void ClearCurrentLine()
-            {
-                var currentLine = Console.CursorTop;
-                Console.SetCursorPosition(0, Console.CursorTop);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, currentLine);
-            }
-
-            var builder = new StringBuilder();
-            var input = Console.ReadKey(intercept:true);
-
-            while (input.Key != ConsoleKey.Enter)
-            {
-                var currentInput = builder.ToString();
-                if (input.Key == ConsoleKey.Tab)
-                {
-                    List<string> data = new List<string>();
-                    string raw = builder.ToString();
-                    string direc = string.Join("\\", raw.Split('\\').Reverse().Skip(1).Reverse()) + "\\";
-                    data = Directory.GetFiles(direc).ToList();
-                    data.AddRange(Directory.GetDirectories(direc));
-                    var match = data.FirstOrDefault(item => item != currentInput && item.StartsWith(currentInput, true, CultureInfo.InvariantCulture));
-                    if (string.IsNullOrEmpty(match))
-                    {
-                        input = Console.ReadKey(intercept: true);
-                        continue;
-                    }
-
-                    ClearCurrentLine();
-                    builder.Clear();
-
-                    Console.Write(match);
-                    builder.Append(match);
-                }
-                else
-                {
-                    if (input.Key == ConsoleKey.Backspace && currentInput.Length > 0)
-                    {
-                        builder.Remove(builder.Length - 1, 1);
-                        ClearCurrentLine();
-
-                        currentInput = currentInput.Remove(currentInput.Length - 1);
-                        Console.Write(currentInput);
-                    }
-                    else
-                    {
-                        var key = input.KeyChar;
-                        builder.Append(key);
-                        Console.Write(key);
-                    }
-                }
-
-                input = Console.ReadKey(intercept:true);
-            }
-            Console.Write(input.KeyChar);
-        }
-    }
-
-    class ImageConverter
-    {
-        private const int char_x = 1;
-        private const int char_y = 2;
-        private const string brightness = "@##$||===;;::''''...  ";
-
-        private ImageConverter() { }
-        public static string GetImage(string file, int parts = 5)
-        {
-            Bitmap image = new Bitmap(Image.FromFile(file));
-            decimal[,] values = new decimal[(int)Math.Ceiling((float)image.Width / char_x / parts), (int)Math.Ceiling((float)image.Height / char_y / parts)];
-            for (int x = 0; x < image.Width; x++)
-            {
-                for (int y = 0; y < image.Height; y++)
-                {
-                    values[(int)Math.Floor((float)x / char_x / parts), (int)Math.Floor((float)y / char_y / parts)] += (decimal)image.GetPixel(x, y).GetBrightness() / parts / parts / char_x / char_y;
-                }
-                Console.Write($"\rprogress: {Math.Ceiling(100f * x / image.Width)}%");
-            }
-            Console.WriteLine();
-            StringBuilder sb = new StringBuilder();
-            for (int y = 0; y < (int)Math.Ceiling((float)image.Height / char_y / parts); y++)
-            {
-                for (int x = 0; x < (int)Math.Ceiling((float)image.Width / char_x / parts); x++)
-                {
-                    sb.Append(brightness[(int)((float)values[x, y] * brightness.Length)]);
-                }
-                sb.Append(Environment.NewLine);
-                Console.Write($"\rprogress: {Math.Ceiling(100f * y / (int)Math.Ceiling((float)image.Height / char_y / parts))}%");
-            }
-            Console.WriteLine("\nDONE!");
-            return sb.ToString();
-        }
+        
     }
 }
